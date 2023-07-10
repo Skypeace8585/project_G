493b029f6c71164a41e56d626960215a4cdef061
allface017 <113155656+allface017@users.noreply.github.com>
2023-07-10 11:59:33 +0900

    フッターの作成#2

---
+++ footer.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+<div class="md-flex md-justify-between">
+  <h1>姫神設備工業</h1>
+  <p>〒020-0102</p>
+  <p>盛岡市上田字岩脇1番地27</p>
+  <p>(盛岡テクノパーク企業団地内)</p>
+  <p>TEL:019-661-8356</p>
+  <p>FAX:019-661-8329</p>
+  <div>
+    <ul class="footer__navi">
+      <li><a href="#">Facebook</a></li>
+      <li><a href="#">Twitter</a></li>
+      <li><a href="#">Instagram</a></li>
+    </ul>
+  </div>
+
+
+    <div class="grid">
+      <div>
+        <ul class="footer__navi">
+          <li><a href="index.php">HOME</a></li>
+          <h2>企業について</h2>
+          <li><a href="enterprise_overview.html">企業概要</a></li>
+          <li><a href="business_achievement_introduction.html">実績</a></li>         
+          <li><a href="Construction_example.html ">施工事例</a></li>
+          <li><a href="employee_introduction.html ">従業員紹介</a></li>
+        </ul>
+        <a href="Recruitment_information.html">採用情報</a>
+      </div>
+      <div>
+        <ul class="footer__navi">
+          <li><a href="notice_list.html">お知らせ</a></li>
+          <li><a href="FAQ.html">よくあるご質問</a></li>
+          <h2>お問い合わせ</h2>
+          <li><a href="repair_point_inquiry.html ">修理・施工などのお問い合わせ</a></li>
+          <li><a href="Recruitment_information_inquiry.html ">採用情報に関するお問い合わせ</a></li>
+          <li><a href="privacy_policy.html">プライバシーポリシー</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <hr/>
+</footer>
