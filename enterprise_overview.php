ed6d52f41a46d6a0ac20c9616d1e079413e7b328
allface017 <113155656+allface017@users.noreply.github.com>
2023-07-20 10:50:24 +0900

    会社概要作成中_enterprise_overview.php(会社概要)ページの作成 #16

---
+++ enterprise_overview.php
@@ -0,0 +1,165 @@
+<!DOCTYdiv class="item"E html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dela+Godtic+One&family=DotGodtic16&family=Mochiy+Pop+One&family=Vollkorn+SC:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/enterprise_overview.css">
+    <title>TOdiv class="item"</title>
+</head>
+<body>
+    <?php require 'header.php'; ?>
+    <h1>会社概要</h1>
+    <dl class="company-overview">
+    <div class="item">
+        <dt>会社名</dt>
+        <dd>株式会社姫神工業</dd>
+    </div class="item">
+    <div class="item">
+        <dt>所在地</dt> 
+        <dd>岩手県盛岡市上田字岩脇1番27</dd>
+    </div class="item">
+    <div class="item">
+        <dt>TEL/FAX</dt> 
+        <dd>019-661-8356 / 019 -661-8329</dd>
+    </div class="item">
+    <div class="item">
+        <dt>メールアドレス </dt>
+        <dd>himekami@mac.ne.jp</dd>
+    </div class="item">
+
+    <div class="item">
+        <dt>代表者</dt> 
+        <dd>長谷川優子(社長)</dd>
+    </div class="item">
+    <div class="item">
+        <dt>創立年月日</dt>  
+        <dd>昭和49年7月1日</dd>
+    </div class="item">
+    <div class="item">
+        <dt>資本金</dt>
+        <dd>11,000 千円</dd> 
+        </div class="item">
+    <div class="item">
+        <dt>建設業許可</dt>
+        <dd>岩手県知事許可 第 03659号</dd> 
+        </div class="item">
+    <div class="item">
+        <dt>資格者一覧</dt>
+        <dd>給水装置工事配管技能者 4名,給水装置工事責任技術者 1名,
+    一級土木施工管理技士 1名,二級管工事施工管理技士 1名,
+    排水設備責任技術者 2名</dd> 
+</div class="item">
+    <div class="item">
+        <dt>従業員数</dt> 
+        <dd>8名</dd>
+    </div class="item">
+    <div class="item">
+        <dt>指名店一覧</dt>
+        <dd>
+            <li>盛岡市(上/下)</li>
+            <li>紫波市(上/下)</li>
+            <li>一戸市(上/下)</li>
+            <li>奥州市(上/下)</li>
+            <li>宮古市(上/下)</li>
+            <li>宮古市(上/下)</li>
+            <li>滝沢市(上/下)</li>
+            <li>一関市(上/下)</li>
+            <li>花巻市(上/下)</li>
+            <li>遠野市(上/下)</li>
+            <li>八幡平市(上/下)</li>
+            <li>住田町(上/下)</li>
+            <li>矢幅町(上/下)</li>
+            <li>矢幅町(上/下)</li>
+            <li>雫石町(上/下)</li>
+            <li>二戸市(上/下)</li>
+            <li>葛巻町(上/下)</li>
+            <li>北上市(上/下)</li>
+            <li>陸前高田市(上/下)</li>
+        </dd>    
+    </div class="item">
+    <div class="item">
+        <dt>主要取引先</dt>
+        <dd>
+            <li>大和ハウス工業(株)</li>
+            <li>大和ライフネクスト(株)</li>
+            <li>セキスイハイム東北(株)</li>
+            <li>睦興産 (有)</li>
+            <li>(株) 興盛工業所</li>
+        </dd>
+    </div class="item">
+    <div class="item">
+        <dt>サービス内容</dt>
+        <dd>土木工事、とび・土木工事,一般住宅・ビル等の採水設備工事,
+            水道施設工事、管工事 </dd>
+        </div class="item">
+    <div class="item">
+        <dt>支払方法</dt>         
+        <dd>現在でのお支払いまたはお振込み(岩手銀行、盛岡信用金庫)</dd>
+    </div class="item">
+    </dl>
+
+    <h1>会社沿革</h1>
+    <dl class="company-overview">
+    <div class="item">
+    <dt>昭和49年 7月</dt>
+    <dd>齋藤莞爾が姫神設備工業を個人創業</dd>
+    </div class="item">
+    <div class="item">
+    <dt>昭和51年 9月</dt>
+    <dd>(有)姫神設備工業設立 </dd>
+    </div class="item">
+    <div class="item">
+    <dt>昭和52年 9月</dt>
+    <dd>建設業許可登録取得</dd>
+    </div class="item">
+    <div class="item">
+    <dt>平成14年 4月</dt>
+    <dd>(株) 姫神設備工業に組織変更   資本金1,100万円に増資  </dd>
+    </div class="item">
+    <div class="item">
+    <dt>平成20年 2月</dt>
+    <dd>事務所移転(盛岡テクノパーク企業団地内) </dd>
+    </div class="item">
+    <div class="item">
+    <dt>令和 2年 7月</dt>
+    <dd>(株)長谷川建設とM&A成約</dd>
+    </div class="item">
+    <div class="item">
+    <dt>令和3年12月</dt>
+    <dd>長谷川優子 代表取締役就任 </dd>
+    </div class="item">
+</dl>
+
+    <h1>交通アクセス</h1>     
+    <div id="map-room">
+        <div class="item">
+        <div class="map-wrap">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12270.532481580525!2d141.1498101!3d39.7478913!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f85743c88fa0a11%3A0x3856036b46a876d4!2z77yI5qCq77yJ5aer56We6Kit5YKZ5bel5qWt!5e0!3m2!1sja!2sjp!4v1689815620569!5m2!1sja!2sjp" 
+                        style="border:0;" 
+                        allowfullscreen="" 
+                        loading="lazy" 
+                        referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
+            </div>
+                    </div>
+            <div class="map-list">
+            <article>
+            <p>所在地</p>
+            <p>〒020-0102</p>
+            <p>盛岡市上田字岩脇1番地27(盛岡テクノパーク企業団地内)</p>
+            <p>アクセス:</p>
+            <p>厨川駅から車で9分</P>
+            <p>松園ターミナルから徒歩５分</P>
+                </article>
+                    </div>
+        <?php require 'footer.php'; ?>
+</body>
+</html>
