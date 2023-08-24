489c79488233ff0b9c066c6664e15e4b66dc3738
allface017 <113155656+allface017@users.noreply.github.com>
2023-08-24 10:30:55 +0900

    実績仮完成

--- business_achievement_introduction.php
+++ business_achievement_introduction.php
@@ -19,29 +19,53 @@
 <ul class="button-Recruit__reverse">
         <li><a href="#Recruit-No1" class="button-Recruit" >一般住宅ビル等の排水設備工事</a></li>
         <li><a href="#Recruit-No2" class="button-Recruit" >水道施設・管工事</a></li>
-        <li><a href="#Recruit-No2" class="button-Recruit" >とび・土木工事</a></li>
+        <li><a href="#Recruit-No3" class="button-Recruit" >とび・土木工事</a></li>
 </ul>
+<section class="Recruit" id="Recruit-No1">
+<div class="Businesses">
+    <h1>一般住宅ビル等の排水設備工事</h1>
+    
+    <p>建物の水に関わる工事全般の設計・施工を行います。
+        給水・給湯設備、排水設備、トイレ・洗面器・ポンプ・タンク
+        などの衛生設備などがあります。  </p>
 
-<h1>一般住宅ビル等の排水設備工事</h1>
-<img></img>
+    <!-- <img src="img/toilet1.jpg" alt="一般住宅ビル等排水設備工事" class="img">
+    <img src="img/toilet2.jpg" alt="一般住宅ビル等排水設備工事" class="img"> -->
 
-<p>建物の水に関わる工事全般の設計・施工を行います。
-    給水・給湯設備、排水設備、トイレ・洗面器・ポンプ・タンク
-    などの衛生設備などがあります。  </p>
+</div>
+</section>
 
+<section class="Recruit" id="Recruit-No2">
+<div class="Businesses">
     <h1>水道施設・管工事</h1>
-<img></img>
+<!-- <img src="img/kantop.jpg" alt="水道施設・管工事" class="img"> -->
+
 
 <p>水道や下水処理場の施設や処理場を構造、設置する工事が水道施設工事で、
     家屋やその他の施設に配水小管を設置する工事。
     これらの敷地外の下水道の配管工事・施工を行います。</p>
 
+    <!-- <img src="img/kan2.jpg" alt="水道施設・管工事" class="img">
+    <img src="img/kan3.jpg" alt="水道施設・管工事" class="img"> -->
+
+
+</div>
+
+</section>
+<section class="Recruit" id="Recruit-No3">
+<div class="Businesses">
     <h1>とび・土木工事</h1>
-<img></img>
+<!-- <img src="img/door1.jpg" alt="とび・土木工事" class="img"> -->
 
-<p>総合的な土木工作を建設する工事（補修・改造・解体含む）の工事を行います。</p>
+<p>水道や下水処理場の施設や処理場を構造、設置する工事が水道施設工事で、
+    家屋やその他の施設に配水小管を設置する工事。
+    これらの敷地外の下水道の配管工事・施工を行います。</p>
+<!-- 
+<img src="img/door2.jpg" alt="とび・土木工事" class="img">
+<img src="img/door3.jpg" alt="とび・土木工事" class="img"> -->
 
-
+</div>
+</section>
 <?php require 'footer.php'; ?>
 </body>
 </html>
